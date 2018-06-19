e571bd045364dc493ea7f0c4ea22cd6a18aed714
Ryanden <lockstom@gmail.com>
2018-06-19 16:38:04 +0900

    multi table

--- app/inheritance/multi_table/models.py
+++ app/inheritance/multi_table/models.py
@@ -15,6 +15,9 @@ class Place(models.Model):
     def __str__(self):
         return f'[{self.pk}] Place({self.name})'
 
+    class Meta:
+        ordering = ['name']
+
 
 class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
@@ -36,3 +39,19 @@ class Supplier(Place):
         on_delete=models.CASCADE,
         related_name='supplier_by_customer'
     )
+
+    # 특정 Place인 p1이 있을 때
+    # Supplier.objects.filter(customer=p1)
+    # Supplier.objects.filter(place_ptr=p1)
+
+    # p1이 역방향 Manager를 사용할 때
+    # related_name 안겹침
+    #   p1.supplier_set <- MTO
+    #   p1.supplier     <- OTO (역방향 매니저가 존재하지 않음)
+
+    # p1이 역뱡향 Query를 사용할 때
+    #  related_name 및 related_query_name에 아무것도 지정하지 않았을때
+    #  related_query_name의 기본값:
+    #    클래스명의 lowercase
+    #       -> supplier (겹침)
+    #  겹치지 않도록 related_name 또는 related_query_name을 지정해준다.
